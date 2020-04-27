7e04e8bb3304b03b268f8d6c3dda85bbdc00585e
Aidan Farhi <Papa@Adias-MacBook-Pro.local>
2020-04-27 13:27:46 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import math
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/total", methods=["POST", "GET"])
+def total():
+    principle = request.form.get("loan_amount")
+    principle = float(principle)
+    rate = request.form.get("percentage")
+    rate = float(rate)
+    rate = rate/100
+    term = request.form.get("term")
+    term = float(term)
+    monthly = principle * rate / (1 - math.pow(1 + rate, - term))
+    return render_template("index.html", monthly="monthly")
+    
+
+
+
+
+
+
+
+
